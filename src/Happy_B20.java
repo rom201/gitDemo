991523bd1f9fde47cdc0f0c7875c33a1f69269c7
rom201 <68832867+rom201@users.noreply.github.com>
2020-08-01 16:37:15 -0400

    Update Happy_B20.java

--- src/Happy_B20.java
+++ src/Happy_B20.java
@@ -2,7 +2,7 @@ public class Happy_B20 {
     public static void main(String[] args) {
 
         System.out.println("This file for day2 Git");
-        System.out.println("task for fetch and merge";
+        System.out.println("task for fetch and merge");
 
     }
 }
