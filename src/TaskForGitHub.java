edbb2cc4a3a1d8d60f22778aebc1968b1517b4fb
rom201 <kovalchukr1104@gmail.com>
2020-08-01 11:56:37 -0400

    apdeded TaskFor

--- src/TaskForGitHub.java
+++ src/TaskForGitHub.java
@@ -1,10 +1,11 @@
 public class TaskForGitHub {
     public static void main(String[] args) {
         System.out.println("This is Git");
-        System.out.println("This is Git");
         System.out.println("This done by August 1");
         System.out.println("Again updated1");
         System.out.println("Again updated2");
+
+        // new line added
 
     }
 }
