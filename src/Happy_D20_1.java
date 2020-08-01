a429932a93b8792f0f244ea375f47490f6f4ec8f
rom201 <kovalchukr1104@gmail.com>
2020-08-01 12:33:50 -0400

    apdated file 3

--- src/Happy_D20_1.java
+++ src/Happy_D20_1.java
@@ -2,6 +2,8 @@ public class Happy_D20_1 {
     public static void main(String[] args) {
 
         System.out.println("this is 3d file we created");
+        System.out.println("this is changes 1");
+        System.out.println("this is changes 2");
 
     }
 }
