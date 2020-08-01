16e19d7ba57f06fb38f77627517ee2a0a7bd0fd2
rom201 <kovalchukr1104@gmail.com>
2020-08-01 15:19:31 -0400

    new file Logout

--- src/Happy_D20_1.java
+++ src/Happy_D20_1.java
@@ -4,6 +4,8 @@ public class Happy_D20_1 {
         System.out.println("this is 3d file we created");
         System.out.println("this is changes 1");
         System.out.println("this is changes 2");
+        System.out.println("this is changes 3");
+        System.out.println("this is changes 4");
 
     }
 }
---
+++ src/LogOut_1.java
@@ -0,0 +1,5 @@
+public class LogOut_1 {
+    public static void main(String[] args) {
+        System.out.println("Logout");
+    }
+}
