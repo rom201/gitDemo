7ff7bf2cdab16d1e5927dcd64c1ee29dfcfcba63
rom201 <kovalchukr1104@gmail.com>
2020-08-01 11:51:56 -0400

    TaskForGit is apdated

--- src/TaskForGitHub.java
+++ src/TaskForGitHub.java
@@ -3,7 +3,8 @@ public class TaskForGitHub {
         System.out.println("This is Git");
         System.out.println("This is Git");
         System.out.println("This done by August 1");
-
+        System.out.println("Again updated1");
+        System.out.println("Again updated2");
 
     }
 }
