bad814c934ac0b4dc5ab076fd789c1909a3b1ed8
rom201 <kovalchukr1104@gmail.com>
2020-08-01 10:47:49 -0400

    commit 08_01

--- src/TaskForGitHub.java
+++ src/TaskForGitHub.java
@@ -2,6 +2,7 @@ public class TaskForGitHub {
     public static void main(String[] args) {
         System.out.println("This is Git");
         System.out.println("This is Git");
+        System.out.println("This done by August 1");
 
 
     }
